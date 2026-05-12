e2243f65fd4c746cf37c6a408ef0d04b42ce41c3
rpostkg <none>
2026-05-12 09:42:14 +0100

    Added a shield icon to the Review & Confirm button

--- Rewind/Views/ReviewChangesDialog.xaml.cs
+++ Rewind/Views/ReviewChangesDialog.xaml.cs
@@ -1,4 +1,6 @@
+using Microsoft.UI.Xaml;
 using Microsoft.UI.Xaml.Controls;
+using Microsoft.UI.Xaml.Media;
 using Rewind.Models;
 using System.Collections.Generic;
 
@@ -9,6 +11,34 @@ public sealed partial class ReviewChangesDialog : ContentDialog
     public ReviewChangesDialog(List<ChangeItem> changes)
     {
         this.InitializeComponent();
+
         ChangesRepeater.ItemsSource = changes;
+
+        this.Opened += ReviewChangesDialog_Opened; // Refer to the comment below.
+    }
+    // The year is 2026 and you still can't easily modify ContentDialog controls.
+    // I really have to do all of this just to add the shield icon to indicate that the action will require admin privileges.
+    private void ReviewChangesDialog_Opened(ContentDialog sender, ContentDialogOpenedEventArgs args)
+    {
+        if (GetTemplateChild("PrimaryButton") is Button primaryButton)
+        {
+            primaryButton.Content = new StackPanel
+            {
+                Orientation = Orientation.Horizontal,
+                Spacing = 8,
+                Children =
+                {
+                    new FontIcon
+                    {
+                        FontFamily = new FontFamily("Segoe MDL2 Assets"),
+                        Glyph = "\uEA18"
+                    },
+                    new TextBlock
+                    {
+                        Text = "Confirm & Apply"
+                    }
+                }
+            };
+        }
     }
 }
