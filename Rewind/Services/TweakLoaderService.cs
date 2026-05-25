3002c3c5ec06a02dd5562d83c5accc93e0db716a
rpostkg <none>
2026-05-25 15:50:51 +0100

    Tweaks can be localized now

--- Rewind/Services/TweakLoaderService.cs
+++ Rewind/Services/TweakLoaderService.cs
@@ -2,6 +2,7 @@ using Rewind.Models;
 using System;
 using System.Collections.Generic;
 using System.IO;
+using System.Linq;
 using YamlDotNet.Serialization;
 using YamlDotNet.Serialization.NamingConventions;
 
@@ -13,7 +14,7 @@ public class TweakLoaderService
     {
         var tweaks = new List<Tweak>();
         var tweaksFolder = Path.Combine(AppDomain.CurrentDomain.BaseDirectory, "Tweaks");
-        
+
         if (!Directory.Exists(tweaksFolder))
             return tweaks;
 
@@ -30,21 +31,65 @@ public class TweakLoaderService
             {
                 var yaml = File.ReadAllText(file);
                 var tweakFile = deserializer.Deserialize<TweakFile>(yaml);
-                
+
                 if (tweakFile?.Tweaks != null)
                 {
                     foreach (var tweak in tweakFile.Tweaks)
                     {
                         if (currentBuild >= tweak.MinVersion && currentBuild <= tweak.MaxVersion)
-                        {
                             tweaks.Add(tweak);
-                        }
                     }
                 }
             }
             catch { }
         }
 
+        var locale = new PreferencesService().LoadPreferences().Language;
+        if (!string.IsNullOrEmpty(locale))
+            ApplyLocale(tweaks, tweaksFolder, locale);
+
         return tweaks;
+    }
+
+    private void ApplyLocale(List<Tweak> tweaks, string tweaksFolder, string locale)
+    {
+        var localeFolder = Path.Combine(tweaksFolder, "Locale", locale);
+        if (!Directory.Exists(localeFolder)) return;
+
+        var deserializer = new DeserializerBuilder()
+            .WithNamingConvention(CamelCaseNamingConvention.Instance)
+            .IgnoreUnmatchedProperties()
+            .Build();
+
+        var byId = tweaks.ToDictionary(t => t.Id);
+
+        foreach (var file in Directory.GetFiles(localeFolder, "*.yaml"))
+        {
+            try
+            {
+                var localeFile = deserializer.Deserialize<TweakLocaleFile>(File.ReadAllText(file));
+                foreach (var entry in localeFile?.Tweaks ?? [])
+                {
+                    if (!byId.TryGetValue(entry.Id, out var tweak)) continue;
+                    if (entry.Name is not null) tweak.Name = entry.Name;
+                    if (entry.Category is not null) tweak.Category = entry.Category;
+                    if (entry.Description is not null) tweak.Description = entry.Description;
+                }
+            }
+            catch { }
+        }
+    }
+
+    private class TweakLocaleEntry
+    {
+        public string Id { get; set; } = "";
+        public string? Name { get; set; }
+        public string? Category { get; set; }
+        public string? Description { get; set; }
+    }
+
+    private class TweakLocaleFile
+    {
+        public List<TweakLocaleEntry> Tweaks { get; set; } = [];
     }
 }
